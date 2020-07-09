a47a1aa61a652a8689f06da1a46c7d9e9563b422
Ryan McNeil <ryan@journeyapps.com>
2020-07-09 14:57:51 -0600

    portflio new + create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,32 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def show
+
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+    
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+
+  end
+
+end
+
+private
+
+# Only allow a list of trusted parameters through.
+def portfolio_params
+  params.require(:portfolio).permit(:title, :subtitle, :body)
 end
