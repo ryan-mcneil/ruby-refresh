0714d14dc4c50ce362224eb9451f501be63c0554
Ryan McNeil <ryan@journeyapps.com>
2020-07-10 10:51:05 -0600

    defaults and memoization with portfolio images

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,8 @@ class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
+    # @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
+    @portfolio_items = Portfolio.all
   end
 
   def angular
