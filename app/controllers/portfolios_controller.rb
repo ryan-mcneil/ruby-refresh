b3a551cd33d139d2cb55e2d0a487695643190a73
Ryan McNeil <ryan@journeyapps.com>
2020-07-10 10:33:49 -0600

    class methods and scopes for models

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,11 @@ class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
+  end
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
   end
 
   def show
