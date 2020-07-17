c2a897390760a288636af67280ba4cbb4e7d5443
Ryan McNeil <ryan@journeyapps.com>
2020-07-16 18:02:22 -0600

    update form/edit/new + update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,13 +26,19 @@ end
 puts "5 skills created"
 
 8.times do |portfolio_item|
-  Portfolio.create!(
+  p = Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
     body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350X200"
   )
+  3.times do |technology|
+    Technology.create!(
+      name: "Technology: #{technology}",
+      portfolio: p
+    )
+  end
 end
 
 1.times do |portfolio_item|
