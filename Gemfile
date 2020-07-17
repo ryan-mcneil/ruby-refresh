28eee1a227a84f0fdbbf273423f43e178d4bee54
Ryan McNeil <ryan@journeyapps.com>
2020-07-17 16:41:00 -0600

    utilize Rails 6.0 syntax: require + use dependencies over gemfile

--- Gemfile
+++ Gemfile
@@ -48,8 +48,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.2'
 
-gem 'bootstrap', '~> 4.5.0'
-gem 'jquery-rails'
+# gem 'bootstrap', '~> 4.5.0'
+# gem 'jquery-rails'
 gem 'petergate', '~> 1.6', '>= 1.6.3'
 gem 'font-awesome-rails', '~> 4.6', '>= 4.6.3.1'
 gem 'kaminari', '~> 1.2.1'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("popper.js")
+require("bootstrap")
+require("html.sortable")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,7 +19,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
