5507cbfb05e69413f5b51903325d2e1130943270
Ryan McNeil <ryan@journeyapps.com>
2020-07-21 22:02:22 -0600

    save position of drag and drop

--- app/javascript/packs/portfolios.js
+++ app/javascript/packs/portfolios.js
@@ -1,8 +1,39 @@
 import sortable from '../html.sortable.js';
 
+const setPostions = () => {
+  $('.card').each( function(i) {
+    $(this).attr('data-pos', i+1)
+  })
+};
+
 $( () => {
-  sortable('.sortable', {
-    forcePlaceholderSize: true,
-    placeholderClass: 'row'
-  });
+  setPostions();
+  if (sortable('.sortable')[0]) {
+    sortable('.sortable', {
+      forcePlaceholderSize: true,
+      placeholderClass: 'row'
+    });
+    sortable('.sortable')[0]
+      .addEventListener('sortupdate', (e, i) => {
+        console.log(e);
+        let updatedOrder = [];
+        setPostions();
+        $('.card').each( function(i) {
+          updatedOrder.push({
+            id: $(this).data("id"),
+            position: i + 1
+          })
+        })
+        console.log('updatedOrder: ', updatedOrder)
+        $.ajax({
+          method: 'PUT',
+          beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+          url: '/portfolios/sort',
+          data: {
+            order: updatedOrder
+          }
+  
+        })
+      })
+  }
 })
