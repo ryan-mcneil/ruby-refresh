fc046af1fc4cf07838eb083d03e1914de9b7f550
Ryan McNeil <ryan@journeyapps.com>
2020-08-22 16:48:17 -0600

    update REDIS localhost and make AWS image more specifc

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -3,7 +3,7 @@ class PortfolioUploader < CarrierWave::Uploader::Base
   storage :aws
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}/rand(100000000000)"
   end
  
   def extension_whitelist
